34c13986173dee877ad7478f74ed46e1db4aa528
Kantslo <gqancliani@gmail.com>
2024-04-01 07:31:13 +0400

    feat: added caching for customer products page

--- app/(customerFacing)/products/page.tsx
+++ app/(customerFacing)/products/page.tsx
@@ -1,13 +1,15 @@
-import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard";
-import db from "@/db/db";
 import { Suspense } from "react";
 
-function getProducts() {
+import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard";
+import { cache } from "@/lib/cache";
+import db from "@/db/db";
+
+const getProducts = cache(() => {
   return db.product.findMany({
     where: { isAvailableForPurchase: true },
     orderBy: { name: "asc" },
   });
-}
+}, ["/products", "getProducts"]);
 
 export default function ProductsPage() {
   return (
