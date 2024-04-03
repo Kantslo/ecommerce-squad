25bec9171845d4be5521d9e1283d2c92dbae786c
Kantslo <gqancliani@gmail.com>
2024-04-03 17:08:14 +0400

    feat: created purchase receipt for email

---
+++ email/PurchaseReceipt.tsx
@@ -0,0 +1,64 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components";
+import { OrderInformation } from "./components/OrderInformation";
+
+type PurchaseReceiptEmailProps = {
+  product: {
+    name: string;
+    imagePath: string;
+    description: string;
+  };
+  order: {
+    id: string;
+    createdAt: Date;
+    pricePaidInCents: number;
+  };
+  downloadVerificationId: string;
+};
+
+PurchaseReceiptEmail.PreviewProps = {
+  product: {
+    name: "Product Name",
+    description: "Description",
+    imagePath:
+      "/products/82c31fc3-eaeb-4156-990e-1dbe801db389-E-commerce-squad.png",
+  },
+  order: {
+    id: crypto.randomUUID(),
+    createdAt: new Date(),
+    pricePaidInCents: 10000,
+  },
+  downloadVerificationId: crypto.randomUUID(),
+} satisfies PurchaseReceiptEmailProps;
+
+export default function PurchaseReceiptEmail({
+  product,
+  order,
+  downloadVerificationId,
+}: PurchaseReceiptEmailProps) {
+  return (
+    <Html>
+      <Preview>Download {product.name} and view receipt</Preview>
+      <Tailwind>
+        <Head />
+        <Body className="font-sans bg-white">
+          <Container className="max-w-xl">
+            <Heading>Purchase receipt</Heading>
+            <OrderInformation
+              order={order}
+              product={product}
+              downloadVerificationId={downloadVerificationId}
+            />
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
