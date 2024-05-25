1a42ca26f3e662ea7e3858f32320fc96b60d0ee0
kantslo <gqancliani@gmail.com>
2024-05-25 23:46:41 +0400

    feat: created chart for orders by date

---
+++ app/admin/_components/charts/OrdersByDateChart.tsx
@@ -0,0 +1,3 @@
+export function OrderByDayChart() {
+  return "Chart";
+}
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/ui/card";
 import db from "@/db/db";
 import { formatCurrency, formatNumber } from "@/lib/formatters";
+import { OrderByDayChart } from "./_components/charts/OrdersByDateChart";
 
 async function getSalesData() {
   const data = await db.order.aggregate({
@@ -56,25 +57,28 @@ export default async function AdminDashboard() {
     getProductData(),
   ]);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <DashboardCard
-        title="Sales"
-        subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
-        body={formatCurrency(salesData.amount)}
-      />
-      <DashboardCard
-        title="Customers"
-        subtitle={`${formatCurrency(
-          userData.averageValuePerUser
-        )} Average Value`}
-        body={formatNumber(userData.userCount)}
-      />
-      <DashboardCard
-        title="Active Products"
-        subtitle={`${formatNumber(productData.inactiveCount)} Inactive`}
-        body={formatNumber(productData.activeCount)}
-      />
-    </div>
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <DashboardCard
+          title="Sales"
+          subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
+          body={formatCurrency(salesData.amount)}
+        />
+        <DashboardCard
+          title="Customers"
+          subtitle={`${formatCurrency(
+            userData.averageValuePerUser
+          )} Average Value`}
+          body={formatNumber(userData.userCount)}
+        />
+        <DashboardCard
+          title="Active Products"
+          subtitle={`${formatNumber(productData.inactiveCount)} Inactive`}
+          body={formatNumber(productData.activeCount)}
+        />
+      </div>
+      <OrderByDayChart />
+    </>
   );
 }
 
