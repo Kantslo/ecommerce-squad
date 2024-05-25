fd481e98f072227569d2a5dbddeeae80efdbefe3
kantslo <gqancliani@gmail.com>
2024-05-26 02:48:37 +0400

    feat: customized and styled total sales data charts

--- app/admin/_components/charts/OrdersByDateChart.tsx
+++ app/admin/_components/charts/OrdersByDateChart.tsx
@@ -1,3 +1,42 @@
-export function OrderByDayChart() {
-  return "Chart";
+"use client";
+
+import { formatCurrency } from "@/lib/formatters";
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+type OrdersByDateChartProps = {
+  data: {
+    date: string;
+    totalSales: number;
+  }[];
+};
+
+export function OrderByDayChart({ data }: OrdersByDateChartProps) {
+  return (
+    <ResponsiveContainer width="100%" minHeight={300}>
+      <LineChart data={data} width={500} height={200}>
+        <CartesianGrid stroke="hsl(var(--muted))" />
+        <XAxis dataKey="date" stroke="hsl(var(--primary))" />
+        <YAxis
+          tickFormatter={(tick) => formatCurrency(tick)}
+          stroke="hsl(var(--primary))"
+        />
+        <Tooltip formatter={(value) => formatCurrency(value as number)} />
+        <Line
+          dot={false}
+          dataKey="totalSales"
+          type="monotone"
+          name="totalSales"
+          stroke="hsl(var(--primary))"
+        />
+      </LineChart>
+    </ResponsiveContainer>
+  );
 }
