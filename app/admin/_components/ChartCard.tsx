5fc8afb4dc60c00c6e970d69c150a57c153aee0d
kantslo <gqancliani@gmail.com>
2024-05-28 18:28:10 +0400

    feat: created dropdown menu for custom range option

--- app/admin/_components/ChartCard.tsx
+++ app/admin/_components/ChartCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -8,10 +8,18 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { RANGE_OPTIONS } from "@/lib/rangeOptions";
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
+import { Calendar } from "@/components/ui/calendar";
+import { after } from "node:test";
+import { DateRange } from "react-day-picker";
+import { subDays } from "date-fns";
 
 type ChartCardProps = {
   title: string;
@@ -29,10 +37,23 @@ export function ChartCard({
   const searchParams = useSearchParams();
   const router = useRouter();
   const pathName = usePathname();
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: subDays(new Date(), 29),
+    to: new Date(),
+  });
 
-  function setRange(range: keyof typeof RANGE_OPTIONS) {
+  function setRange(range: keyof typeof RANGE_OPTIONS | DateRange) {
     const params = new URLSearchParams(searchParams);
-    params.set(queryKey, range);
+    if (typeof range === "string") {
+      params.set(queryKey, range);
+      params.delete(`${queryKey}To`);
+    } else {
+      if (range.from == null || range.to == null) return;
+
+      params.delete(queryKey);
+      params.set(`${queryKey}From`, range.from.toISOString());
+      params.set(`${queryKey}To`, range.to.toISOString());
+    }
     router.push(`${pathName}?${params.toString()}`, { scroll: false });
   }
 
@@ -53,6 +74,33 @@ export function ChartCard({
                   {value.label}
                 </DropdownMenuItem>
               ))}
+              <DropdownMenuSeparator />
+              <DropdownMenuSub>
+                <DropdownMenuSubTrigger>Custom</DropdownMenuSubTrigger>
+                <DropdownMenuSubContent>
+                  <div>
+                    <Calendar
+                      mode="range"
+                      disabled={{ after: new Date() }}
+                      selected={dateRange}
+                      defaultMonth={dateRange?.from}
+                      onSelect={setDateRange}
+                      numberOfMonths={2}
+                    />
+                    <DropdownMenuItem className="hover:bg-auto">
+                      <Button
+                        onClick={() => {
+                          if (dateRange == null) return;
+                          setRange(dateRange);
+                        }}
+                        disabled={dateRange == null}
+                        className="w-full">
+                        Submit
+                      </Button>
+                    </DropdownMenuItem>
+                  </div>
+                </DropdownMenuSubContent>
+              </DropdownMenuSub>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
